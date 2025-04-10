0f4c81546317fa533ef3288b183c6dddbb908455
4b2g0036@office.stust.edu.tw <4b2g0036@office.stust.edu.tw>
2025-04-11 01:50:39 +0800

    更新關機功能及新增螢幕關閉選項
    更改 `shutdown.bat` 檔案路徑，並新增創建目錄的功能。當使用者輸入的時間為零時，程式會取消關機並創建新的 `AutoShutdown.bat` 檔案，寫入 `shutdown -a` 指令；若時間不為零，則寫入 `shutdown -s -t` 指令並執行。新增詢問使用者是否要關閉螢幕的功能，若選擇是，則創建並執行 `Screenoff.bat` 檔案。最後，程式會刪除指定的資料夾。

--- Project1/AutoSutdown.cpp
+++ Project1/AutoSutdown.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main() {
-	FILE* fp;
-	errno_t err = fopen_s(&fp, "F:/Item/shutdown.bat", "w");
-	if (err != 0) {
+	system("mkdir Progress");
+	
+	FILE* fp1;
+	errno_t err1 = fopen_s(&fp1, "E:/Gamesfolder/Vs/AutoShutdown/Project1/Progress/AutoShutdown.bat", "w");
+	if (err1 != 0) {
 		cerr << "Failed to open file" << endl;
 		return 1;
 	}
@@ -16,9 +18,9 @@ int main() {
 	cin >> input;
 	time = input * sec;
 	if (time == 0) {
-		fprintf(fp, "shutdown -a");
-		fclose(fp);
-		if (system("F:/Item/shutdown.bat") == 1116) {
+		fprintf(fp1, "shutdown -a");
+		fclose(fp1);
+		if (system("E:/Gamesfolder/Vs/AutoShutdown/Project1/Progress/AutoShutdown.bat") == 1116) {
 			cout << "No shutdown in progress" << endl;
 		}
 		else {
@@ -26,11 +28,28 @@ int main() {
 		}
 	}
 	if (time != 0) {
-		fprintf(fp, "shutdown -s -t %d", (int)time);
-		fclose(fp);
-		system("F:/Item/shutdown.bat");
+		fprintf(fp1, "shutdown -s -t %d", (int)time);
+		fclose(fp1);
+		system("E:/Gamesfolder/Vs/AutoShutdown/Project1/Progress/AutoShutdown.bat");
 		cout << "Shutdown in " << input << " hour" << endl;
 	}
-	system("pause");
+	cout << "Turn off the screen or not? (y/n): ";
+	char choice;
+	cin >> choice;
+	if (choice == 'y' || choice == 'Y') {
+		FILE* fp2;
+		errno_t err2 = fopen_s(&fp2, "E:/Gamesfolder/Vs/AutoShutdown/Project1/Progress/Screenoff.bat", "w");
+		if (err2 != 0) {
+			cerr << "Failed to open file" << endl;
+			return 1;
+		}
+		fprintf(fp2, "@echo off\npowershell (Add-Type '[DllImport(\\\"user32.dll\\\")]^public static extern int PostMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::PostMessage(-1,0x0112,0xF170,2)");
+		fclose(fp2);
+		system("E:/Gamesfolder/Vs/AutoShutdown/Project1/Progress/Screenoff.bat");
+	}
+	else {
+		cout << "Good Luck" << endl;
+	}
+	system("rmdir /s /q \"E:/Gamesfolder/Vs/AutoShutdown/Project1/AutoShutdown\"");
 	return 0;
 }
